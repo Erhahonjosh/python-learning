3bb9373a2a0c2aa184de1e3b580e2714fd8df871
Erhahonjosh <erhahonjosh@gmail.com>
2025-12-21 15:04:31 +0100

    Day 3: conditions and decision making

---
+++ day3.py
@@ -0,0 +1,21 @@
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+number = int(input("Enter a number: "))
+if number % 2 ==0:
+    print("This number is even")
+else:
+    print("This number is odd")
+
+score = int(input("Enter your score: "))
+if score >= 70:
+    print("Grade: A")
+elif score >= 60:
+    print("Grade: B")
+elif score >= 50:
+    print("Grade: C")
+else:
+    print("Fail")
